fd839fa7641de1c7e40333cf2db9337d24f9c994
Rushwan07 <rushdeveloper07@gmail.com>
2024-09-02 19:13:24 +0530

    Admin Tranportation Updated

--- src/models/transportationModel.js
+++ src/models/transportationModel.js
@@ -14,15 +14,24 @@ const localizedString = {
   },
 };
 
-// Define the transportation schema
 const transportationModel = new mongoose.Schema(
   {
     coverImage: {
       type: String,
       required: [true, "Please provide a cover image for the tour transportation"],
     },
+
     title: localizedString, // Title with localization
-    places: [localizedString], // Places array with localization
+    type: localizedString,
+    other: localizedString,
+    passenger: { // Updated field name
+      type: Number,
+      required: [true, "Please provide a number of passengers for the tour transportation"],
+    },
+    luggage: {
+      type: Number,
+      required: [true, "Please provide a luggage for the tour transportation"],
+    },
     isActive: {
       type: Boolean,
       default: true,
@@ -30,6 +39,7 @@ const transportationModel = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
 
 // Create the model from the schema
 const Transportation = mongoose.model("Transportation", transportationModel);
