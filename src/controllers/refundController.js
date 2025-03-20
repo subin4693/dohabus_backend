949c252ebb1b1b93beec26a1b698a087155c1ad7
Melvin <melvinmps11301@gmail.com>
2025-03-20 11:19:42 +0300

    currency changed to small letters

--- src/controllers/refundController.js
+++ src/controllers/refundController.js
@@ -595,14 +595,13 @@ exports.processRefund = catchAsync(async (req, res, next) => {
     // Production endpoint: https://api.cybersource.com/pts/v2/payments/{id}/refunds
     // For testing, use: https://apitest.cybersource.com/pts/v2/payments/{id}/refunds
     const refundEndpoint = `https://api.cybersource.com/pts/v2/payments/${ticket.cybersourceOrderId}/refunds`;
-    console.log(refundEndpoint);
 
     // Build payload per the REST API structure
     const refundPayload = {
       orderInformation: {
         amountDetails: {
           totalAmount: refundAmount.toString(),
-          currency: "QAR", // e.g., "USD"
+          currency: "qar", // e.g., "USD"
         },
       },
     };
